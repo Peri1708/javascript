e81d875f37554eae0d9feab795bb0d8680e4be87
Peri1708 <mylap1695@gmail.com>
2025-07-02 00:34:25 +0530

    Add files via upload
    Calculator operation in js

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function game()
+{
+    let num1=parseInt(prompt("Enter a number : "));
+    let num2=parseInt(prompt("Enter a number : "));
+    let sym=prompt("Enter a sign : ");
+    if(sym=='+')
+    {
+        console.log("The sum of numbers is : "+(num1+num2));
+    }
+    else if(sym=='-')
+    {
+        console.log("The difference of numbers is : "+(num1-num2));
+    }
+    else if(sym=='*')
+    {
+        console.log("The sum of numbers is : "+(num1*num2));
+    }
+    
+}
